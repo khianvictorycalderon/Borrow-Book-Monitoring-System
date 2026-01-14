f4e69a8568deb2ed07dafa86737cf7ea7a3c561c
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-14 12:59:06 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Borrow Book Monitoring System
+An internal web utility system that monitors and tracks who borrowed which book.
+
+## Prerequisites
+- Apache
+- MySQL Server
+I recommend using XAMPP as it comes with built-in Apache for PHP and MySQL Server.
+
+## Setup
+1. Go to you XAMPP htdocs directory (usually in `C:\xampp\htdocs`, depending on where you installed your XAMPP)
+2. Clone this repository by running `git clone https://github.com/khianvictorycalderon/Borrow-Book-Monitoring-System.git`
+3. Start Apache and MySQL from XAMPP control panel.
+4. Go to your phpmyadmin panel and run this MySQL query:
+    ```sql
+    -- SQL Query here...
+    ```
+5. Go to your browser and run `localhost/Borrow-Book-Monitoring-System`
+6. Enjoy!
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+  require_once("db.php");
+  
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    
+  	<meta http-equiv='cache-control' content='no-cache'> 
+    <meta http-equiv='expires' content='0'> 
+    <meta http-equiv='pragma' content='no-cache'>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/png+jpg" href="/images/icons/book-borrow-monitoring-system.png">
+    <script src="/assets/tailwind-3.4.17.js"></script>
+    <script type="module" src="/assets/main.js"></script>
+    
+    <title>Borrow Book Monitoring System</title>
+
+  </head>
+  <body>
+
+    <div class="min-h-screen w-full 
+        bg-neutral-50 text-neutral-900
+        dark:bg-neutral-900 dark:text-neutral-50
+        flex items-center justify-center
+        ">
+        
+        <div class="flex flex-col gap-2 text-center">
+            <h2 class="text-4xl font-bold text-center">Borrow Book Monitoring System</h2>
+            <p class="text-sm italic text-center">with PHP + Tailwind CSS + MySQL</p>
+        </div>
+        
+    </div>
+
+  </body>
+</html>
